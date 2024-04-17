0be17cfcf28fa6b2a6e88dc98848eae3142f5624
Chris Ge <chris.ge@live.com>
2024-04-17 16:26:43 -0400

    Add some scripts to test git connection in vertex workbench

---
+++ Automated-Testing-for-LLMOps/utils.py
@@ -0,0 +1,142 @@
+import github
+import os
+import requests
+import random
+from dotenv import load_dotenv, find_dotenv
+from yaml import safe_dump, safe_load
+
+adjectives = [
+    "adoring",
+    "affirmative",
+    "appreciated",
+    "available",
+    "best-selling",
+    "blithe",
+    "brightest",
+    "charismatic",
+    "convincing",
+    "dignified",
+    "ecstatic",
+    "effective",
+    "engaging",
+    "enterprising",
+    "ethical",
+    "fast-growing",
+    "glad",
+    "hardy",
+    "idolized",
+    "improving",
+    "jubilant",
+    "knowledgeable",
+    "long-lasting",
+    "lucky",
+    "marvelous",
+    "merciful",
+    "mesmerizing",
+    "problem-free",
+    "resplendent",
+    "restored",
+    "roomier",
+    "serene",
+    "sharper",
+    "skilled",
+    "smiling",
+    "smoother",
+    "snappy",
+    "soulful",
+    "staunch",
+    "striking",
+    "strongest",
+    "subsidized",
+    "supported",
+    "supporting",
+    "sweeping",
+    "terrific",
+    "unaffected",
+    "unbiased",
+    "unforgettable",
+    "unrivaled",
+]
+
+nouns = [
+    "agustinia",
+    "apogee",
+    "bangle",
+    "cake",
+    "cheese",
+    "clavicle",
+    "client",
+    "clove",
+    "curler",
+    "draw",
+    "duke",
+    "earl",
+    "eustoma",
+    "fireplace",
+    "gem",
+    "glove",
+    "goal",
+    "ground",
+    "jasmine",
+    "jodhpur",
+    "laugh",
+    "message",
+    "mile",
+    "mockingbird",
+    "motor",
+    "phalange",
+    "pillow",
+    "pizza",
+    "pond",
+    "potential",
+    "ptarmigan",
+    "puck",
+    "puzzle",
+    "quartz",
+    "radar",
+    "raver",
+    "saguaro",
+    "salary",
+    "sale",
+    "scarer",
+    "skunk",
+    "spatula",
+    "spectacles",
+    "statistic",
+    "sturgeon",
+    "tea",
+    "teacher",
+    "wallet",
+    "waterfall",
+    "wrinkle",
+]
+
+
+def inspect_config():
+  with open("circle_config.yml") as f:
+    print(safe_dump(safe_load(f)))
+
+
+def load_env():
+  _=load_dotenv(find_dotenv())
+
+
+def get_vertex_api_key():
+  load_env()
+  return os.getenv("VERTEX_API_KEY")
+
+def get_circle_api_key():
+  load_env()
+  circle_token = os.getenv("CIRCLE_TOKEN")
+  return circle_token
+
+def get_gh_api_key():
+  load_env()
+  github_token = os.getenv("GH_TOKEN")
+  return github_token
+
+def get_repo_name():
+  return "ChrisGe4/automated-testing-for-llmops"
+
+
+def
